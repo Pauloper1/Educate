f4059782315828f6d268fb54132e489127526e8b
Pauloper1 <paulcrescent.p@gmail.com>
2024-06-25 01:11:03 +0530

    on click added on section_1 image

--- src/components/Section_1.jsx
+++ src/components/Section_1.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './base.css'
+import 'react-modal-video/scss/modal-video.scss';
+import ModalVideo from 'react-modal-video';
+
+
 function Section_1() {
+    const [isOpen, setOpen] = useState(false)
     return (
         <>
             <div className="container mt-3">
@@ -9,10 +14,21 @@ function Section_1() {
                         <h1 className='text-center mt-3 custom-font'>Get The Best Online Learning Service</h1>
                         <p className='text-center'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, exercitationem reiciendis. Quibusdam sequi ipsa corrupti nemo, dicta accusamus nesciunt tenetur.</p>
                         <button className='btn btn-primary my-3'>Start a course</button>
-                        <div className="continer">
-                            <img src="\images\image1.png" className='img-fluid rounded' alt="" />
+                        <div className="continer" style={{cursor: 'pointer'}}>
+                            <img onClick={() => setOpen(true)} src="\images\image1.png" className='img-fluid rounded' alt="" />
                         </div>
                     </div>
+                    <ModalVideo
+                        channel="youtube"
+                        youtube={{ mute: 0, autoplay: 0 }}
+                        isOpen={isOpen}
+                        videoId="4h8B080Mv4U"
+                        onClose={() => setOpen(false)}
+                    />
+                    {/* <button className="btn-primary" >
+                        VIEW DEMO
+                    </button> */}
+
 
                 </div>
             </div>
